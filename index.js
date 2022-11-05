d7742fe11a4a2ad8308c23574cf8f98fd00a5eed
amJayem <csejayem19@gmail.com>
2022-11-05 14:37:18 +0600

    dotenv updated

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require("mongodb");
+require('dotenv').config();
 
 const app = express();
 const port = 5000;
@@ -10,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 const uri =
-  "mongodb+srv://jayem:jayem475767@cluster0.42e2srw.mongodb.net/?retryWrites=true&w=majority";
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.42e2srw.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
