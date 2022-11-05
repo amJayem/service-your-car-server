0a230c383c6c0ae063bc7dd79c59fdb37b85e6ac
amJayem <csejayem19@gmail.com>
2022-11-05 14:33:16 +0600

    mongodb initial setup

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+const app = express();
+const port = 5000;
+
+// middle wares
+app.use(cors());
+app.use(express.json());
+
+const uri =
+  "mongodb+srv://jayem:jayem475767@cluster0.42e2srw.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    const serviceCollection = client.db("geniusCarService").collection("services");
+    
+    app.get('/services', async(req,res)=>{
+        const query = {};
+        const cursor = serviceCollection.find(query);
+        const service = await cursor.toArray();
+
+        res.send(service);
+    })
+  }
+  
+  finally { }
+}
+
+run().catch((e) => console.error(e));
+
+app.get("/", (req, res) => {
+  res.send("running server...");
+});
+
+app.listen(port, () => {
+  console.log("running: ", port);
+});
